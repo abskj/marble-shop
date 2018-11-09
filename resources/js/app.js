57bf0c4547c576a95fb4a8b6f122e61041eddb1c
Abhishek Jha <j7.abhi@gmail.com>
2018-11-09 16:12:09 +0530

    init

---
+++ resources/js/app.js
@@ -0,0 +1,36 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+import Main from './components/main.vue'
+// require('./../materialize-src/js/bin/materialize');
+import VueRouter from 'vue-router';
+window.Vue = require('vue');
+Vue.use(VueRouter);
+/**
+ * the vuejs router
+ */
+const routes = [
+    { path: '', component: Main,
+    children:[
+      
+      
+    ]
+  },
+    
+   
+  ];
+  
+  const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+  });
+
+  
+Vue.component('example-component', require('./components/main.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/js/components/main.vue
@@ -0,0 +1,13 @@
+<template>
+   <div class="container">
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non reprehenderit distinctio expedita sit unde dolorem consequuntur animi, eos error cupiditate veniam dignissimos sequi, perferendis quae? Quod temporibus sapiente voluptatem molestias!
+   </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
