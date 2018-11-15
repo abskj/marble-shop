d80b5718cc8435745d5ed8a6ee95bee277db45c7
Abhishek Jha <j7.abhi@gmail.com>
2018-11-15 16:43:14 +0530

    Company Api working

--- app/Http/Controllers/companyController.php
+++ app/Http/Controllers/companyController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\company;
+
 
 class companyController extends Controller
 {
     //
+    public function create(Request $request){
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+        $comp = new company([
+            'name' => $request->input('name')
+        ]);
+        $comp->save();
+        return response()->json([
+            'code' => 1,
+            'text' => 'Company added successfully'
+        ]);
+    }
+
+    public function delete(Request $request){
+        $this->validate($request, [
+            'id' => 'required'
+        ]);
+        $comp = company::find($request->input('id'));
+        $comp-> delete();
+        return response()->json([
+            'code' => 1,
+            'text' => 'Company deleted successfully'
+        ]);
+    }
+
+    public function fetch(Request $request){
+        $company_data = company::all();
+        return response()->json([
+            'companies' => $company_data
+        ]);
+    }
 }
